d27ae29932926d2b42e2b06e0fd58548e0f06508
raziki20 <57464663+raziki20@users.noreply.github.com>
2022-05-29 15:39:22 +0700

    v.1.0

---
+++ view/p_daftar.php
@@ -0,0 +1,23 @@
+<?php
+include 'koneksi.php';
+
+if (@$_POST['simpan']) {
+
+  $nama = @$_POST['nama'];
+  $email = @$_POST['email'];
+  $username = @$_POST['username'];
+  $password = password_hash(@$_POST['username'], PASSWORD_DEFAULT);
+  // $encrp = sha1($password);
+
+  mysqli_query($connect, "INSERT INTO user(nama,email,username,password) VALUES ('$nama','$email','$username', '$password')");
+
+?>
+
+<script type="text/javascript">
+  alert("Berhasil Daftar");
+  window.location.href="index.php"
+
+</script>
+
+<?php  }
+ ?>
